2ba4439a9c2176b1994c81fd66570b4002910cee
Shridhar Patil <109330251+Micekey@users.noreply.github.com>
2023-02-09 19:14:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function updateClock() {
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
     
-    h = h>10 ? "0"+h : h;
-    m = m>10 ? "0"+m : m;
-    s = s>10 ? "0"+s : s;
+    h = h<10 ? "0"+h : h;
+    m = m<10 ? "0"+m : m;
+    s = s<10 ? "0"+s : s;
 
     hour.innerText = h;
     minute.innerText = m;
